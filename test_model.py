9c16e80c634ba75ca07bc4e779b76049fa9249a0
weiwentao <weiwentao@em-data.com.cn>
2019-08-14 09:53:25 +0800

    test

--- test_model.py
+++ test_model.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 import torch
 import torch.optim as optim
 from PIL import Image
+import pdb
+import numpy as np
 '''
 pytorch单张图片分类
 '''
@@ -25,24 +27,25 @@ transforms=transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
-
+# 单张图片
 img = default_loader('data/wcedata/val/colon/colon.47.jpg')
 img = transforms(img)
 img = img.unsqueeze(0)
 
-input = torch.tensor(img)
+img1 = default_loader('data/wcedata/val/bleeding/bleeding.47.jpg')
+img1 = transforms(img1)
+img1 = img1.unsqueeze(0)
+
+imgs = np.concatenate((img,img1))
+
+# numpy转成Tensor
+input = torch.tensor(imgs)
+
+# 多张图片
+# pdb.set_trace()
 input.to(device)
 model_ft.to(device)
 output = model_ft(input)
 _, preds = torch.max(output, 1)
 print(preds)
 
-# model_ft = model_ft.to(device)
-
-# criterion = nn.CrossEntropyLoss()
-
-# Observe that all parameters are being optimized
-# optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
-
-# Decay LR by a factor of 0.1 every 7 epochs
-# exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
